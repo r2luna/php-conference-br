385036a94632da41305ecfdacdb89563bde8332d
Rafael Lunardelli <rafael@devsquad.com>
2020-12-03 14:54:50 -0700

    wip

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+    <title>PHP Conference Brasil</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
