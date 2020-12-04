58395f68b7fcfc3d8edf11ee097e1c3e70013afd
Rafael Lunardelli <rafael@devsquad.com>
2020-12-03 23:56:49 -0700

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -12,31 +12,7 @@
 </head>
 
 <body>
-    <div class="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
-        <div class="relative max-w-xl mx-auto">
-            <svg class="absolute left-full transform translate-x-1/2" width="404" height="404" fill="none"
-                viewBox="0 0 404 404" aria-hidden="true">
-                <defs>
-                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-            </svg>
-            <svg class="absolute right-full bottom-0 transform -translate-x-1/2" width="404" height="404" fill="none"
-                viewBox="0 0 404 404" aria-hidden="true">
-                <defs>
-                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20"
-                        patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                    </pattern>
-                </defs>
-                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-            </svg>
-            {{ $slot }}
-        </div>
-    </div>
+{{ $slot }}
 </body>
 
 </html>
