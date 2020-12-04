40867bdec35cfc8ad48333058eb1f5d165d7a5b5
Rafael Lunardelli <rafael@devsquad.com>
2020-12-04 02:45:25 -0700

    wip

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,20 @@ class Contact extends Model
     public function messages()
     {
         return $this->hasMany(Message::class);
+    }
+
+    public function scopeWithLastMessage(Builder $query)
+    {
+        $query->addSelect([
+            'last_message' => Message::query()->select('message')
+                ->whereColumn('contact_id', 'contacts.id')
+                ->latest()
+                ->limit(1),
+            'last_time'    => Message::query()->select('created_at')
+                ->whereColumn('contact_id', 'contacts.id')
+                ->latest()
+                ->limit(1),
+        ])
+            ->withCasts(['last_time' => 'datetime']);
     }
 }
--- resources/views/components/whatsapp/contacts/search.blade.php
+++ resources/views/components/whatsapp/contacts/search.blade.php
@@ -1,2 +1,3 @@
 <input type="text" class="w-full px-6 py-2 text-sm border-0 rounded-full"
        placeholder="Search or start new chat" {{ $attributes }}/>
+
